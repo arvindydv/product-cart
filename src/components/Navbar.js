ea176d86c46d2d746ec3e4c85d052c0b36eb6e13
arvindydv <arvindydv03@gmail.com>
2024-03-12 19:16:34 +0530

    completed

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import "./Navbar.css";
+import { FaCartPlus } from "react-icons/fa6";
+
+function Navbar({ productCount }) {
+  return (
+    <nav>
+      <div>
+        <h1>UseReducer</h1>
+      </div>
+      <div className="icon-and-count">
+        <FaCartPlus className="navbar-icon" />
+        <p className="item-count">{productCount > 0 ? productCount : 0}</p>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
